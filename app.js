ecbf965ceb3b773e0210651bc5409356dc82b89e
tejakiran2 <76155086+tejakiran2@users.noreply.github.com>
2023-07-03 02:30:10 +0530

    app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+const http= require('http')
+const fs = require('fs')
+const filecontent =fs.readFileSync('index.html')
+
+const server =http.createServer(req, res=>{
+	res.writeHead(200,{'content-type':'text/html'});
+	res.end(filecontent)
+
+})
+server.listen(8000,'54.86.7.193',()=>{
+	console.log("listening on port 80")
+})
 
