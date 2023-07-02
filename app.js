8528345af2059cac9cd948bf99a28f96757be7c1
tejakiran2 <76155086+tejakiran2@users.noreply.github.com>
2023-07-03 02:50:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-const http= require('http')
-const fs = require('fs')
-const filecontent =fs.readFileSync('index.html')
-
-const server =http.createServer(res=>{
-	res.writeHead(200,{'content-type':'text/html'});
-	res.end(filecontent)
-
-})
-server.listen(8000,'54.86.7.193' );
-console.log('connected');
-
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+  fs.readFile('index.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
