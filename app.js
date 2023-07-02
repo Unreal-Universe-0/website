024701c90661288d03d1f19cceb27d695ee697af
tejakiran2 <76155086+tejakiran2@users.noreply.github.com>
2023-07-03 02:45:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http= require('http')
 const fs = require('fs')
 const filecontent =fs.readFileSync('index.html')
 
-const server =http.createServer( res=>{
+const server =http.createServer( req,res=>{
 	res.writeHead(200,{'content-type':'text/html'});
 	res.end(filecontent)
 
